ceceee74b07f2ac18e4369cc9c5a8b0e081485cb
228miles <hangdothiduc@gmail.com>
2015-11-16 12:37:06 -0500

    first commit

--- public/js/controller/buy.js
+++
@@ -1,8 +0,0 @@
-var angular = angular || {};
-
-angular.module('app.controller.buy', [])
-.controller('BuyCtrl', function($scope, $rootScope, $location, $socket, $state) {
-	console.log("BuyCtrl.");
-    $rootScope.title = $location.url();
-    
-});
--- public/js/controller/cart.js
+++
@@ -1,8 +0,0 @@
-var angular = angular || {};
-
-angular.module('app.controller.cart', [])
-.controller('CartCtrl', function($scope, $rootScope, $location, $socket, $state, $window) {
-	console.log("CartCtrl.");
-    $rootScope.title = $location.url();
-
-});
